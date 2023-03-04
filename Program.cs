da446faa6a0a1858f4c8c208ab5c48ceb24c007c
Gustavo <gustavopregigueiro6@gmail.com>
2023-03-03 21:30:18 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Minhas frutas favoritas");
+Console.WriteLine("> Amora");
+Console.WriteLine("> Melancia");
+Console.WriteLine("> Banana");
+Console.WriteLine("> Morango");
+Console.WriteLine("> Manga");
+Console.WriteLine("> Mamão");
